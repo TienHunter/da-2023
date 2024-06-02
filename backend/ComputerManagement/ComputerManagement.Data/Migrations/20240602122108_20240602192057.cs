6130cabde0b83b1920db1f75384905f3f3e14713
vdtien <tienkbtnhp@gmail.com>
2024-06-02 19:21:36 +0700

    ....

---
+++ backend/ComputerManagement/ComputerManagement.Data/Migrations/20240602122108_20240602192057.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ComputerManagement.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class _20240602192057 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ContentType",
+                table: "agent",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FileName",
+                table: "agent",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Size",
+                table: "agent",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContentType",
+                table: "agent");
+
+            migrationBuilder.DropColumn(
+                name: "FileName",
+                table: "agent");
+
+            migrationBuilder.DropColumn(
+                name: "Size",
+                table: "agent");
+        }
+    }
+}
